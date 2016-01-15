8fac1fb7cc87fdf85098bc1223597a5f23f27505
Edward Hu <edward.s.hu.cs@gmail.com>
2016-01-14 23:14:28 -0800

    changed port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var main = require('./src/main.js');
 var app = express();
 
+var port = process.env.PORT || 3000;
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -49,7 +50,7 @@ app.post('/api', function(req, res) {
 
 
 });
-var server = app.listen(process.env.port, function() {
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
