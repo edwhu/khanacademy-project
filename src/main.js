66305d2cd2683109a95b90cc8781966ac4c082ce
Edward Hu <edward.s.hu.cs@gmail.com>
2016-01-15 10:31:15 -0800

    comments

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var esprima = require('esprima');
 var acorn = require('acorn')
 var _ = require('lodash');
+//acorn and esprima are exchangable. I chose to use esprima.
 
 // Conditions:
 // Whitelist contains code structures that are allowed. Example:
@@ -20,11 +21,8 @@ function parse(string, conditions, callback) {
   var tree = esprima.parse(string);
 
   //console.log(JSON.stringify(tree, null, '\t'));
-  //explore the tree and check for conditions
   var whiteResult = 0;
   var blackResult = 0;
-
-
   var structureArray = [];
 
   function recursiveExplorer(obj, level, current) {
@@ -57,6 +55,12 @@ function parse(string, conditions, callback) {
   structureArray.splice(0, 1);
   //console.log(structureArray, structureArray.length);
 
+  //we need to separate the code blocks now. We separate the structure array
+  //into subarrays by reducing it. Each code block must have objects from level
+  //1 to whatever. Therefore we will split up the structureArray by level.
+
+  //First start by finding all the elements with level 1. Push their indexes into
+  //array foo.
   var foo = [];
   structureArray.reduce(function(a, e, i) {
     if (e.level === 1) {
@@ -65,32 +69,38 @@ function parse(string, conditions, callback) {
     return a;
   }, foo);
 
+  //slice the array into pieces specified by the indexes in the foo array.
   structureArray = foo.map(function(curr, i) {
     if (i + 1 <= foo.length) {
       return structureArray.slice(foo[i], foo[i + 1]);
     }
   })
   console.log(structureArray);
-  //push in whitelist / blacklist / structure results
+  //by default the structure boolean is true. This is because the default condition for
+  //structure is an empty array (meaning the tester doesn't care about structure )
   var booo = true;
-      //we need to check if EVERY codeblock in the structureList exists in some form in the structureArray. if so, then we have a valid structure.
+  //If there is something in the strutureList, then we need to check for matching structures.
+  //we need to check if EVERY codeblock in the structureList exists in some
+  // form in the structureArray. if so, then we have a valid structure.
   if (structureList.length != 0) {
-    var booo = structureList.every(function(codeBlock){
-      return structureArray.some(function(goodCodeBlock){
-        return _.isEqual(codeBlock,goodCodeBlock);
+    var booo = structureList.every(function(codeBlock) {
+      return structureArray.some(function(goodCodeBlock) {
+        return _.isEqual(codeBlock, goodCodeBlock);
       })
     });
 
   }
+  //assemble the final results object
   var results = {
     whitelist: whiteResult,
     blacklist: blackResult,
     structure: booo,
-    tree:tree
+    tree: tree
   };
   callback(results);
 }
 
+//test code. Uncomment below and run node main.js
 // parse('if(5)', {
 //   whitelist: ['Literal'],
 //   blacklist: ['ForStatement'],
